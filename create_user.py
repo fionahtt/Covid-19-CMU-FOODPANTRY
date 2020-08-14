8925569cd45e82e8013d39112c1896691d64f306
Fiona Thendean <fionahtt@gmail.com>
2020-08-13 18:59:58 -0700

    users in database

--- create_user.py
+++ create_user.py
@@ -1,6 +1,15 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskTest import Users
+from flaskTest import database, Users
 
-user = Users("Alex", "asdf@gmail.com", "asdf")
+alex = Users("Alex", "alexli2468@gmail.com", "zsdc")
+fiona = Users("Fiona", "fionahtt@gmail.com", "12345")
+sara = Users("Sara", "sarasong4@gmail.com", "website") 
+
+admin = [alex, fiona, sara]
+for acc in admin:
+    exists = Users.query.filter(Users.email == acc.email).first()
+    if not exists:
+    	database.session.add(acc)
+    	database.session.commit()
